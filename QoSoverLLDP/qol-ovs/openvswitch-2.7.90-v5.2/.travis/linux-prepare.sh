6ecd7288ac503adce8542c227c5b18401811a89b
mannuan <mannuan@mannuan.mannuan>
2017-08-05 14:09:18 +0800

    v5.2

---
+++ QoSoverLLDP/qol-ovs/openvswitch-2.7.90-v5.2/.travis/linux-prepare.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+git clone git://git.kernel.org/pub/scm/devel/sparse/chrisl/sparse.git
+cd sparse && make && make install && cd ..
+
+pip install --disable-pip-version-check --user six flake8 hacking
+pip install --user --upgrade docutils
